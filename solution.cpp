4d996d3520718d4285f9724b0ca2bc78db9c5b24
not <not@localhost.localdomain>
2016-12-31 15:01:02 +0300

    Added removing outer brackets

--- solution.cpp
+++ solution.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-string python_pow_to_math_pow(string s) {
+string python_pow_to_math_pow(const string &s) {
   string res;
   for (int i = 0; i < (int)s.size(); ++i) {
     if (i + 1 < (int)s.size() && s[i] == '*' && s[i + 1] == '*')
@@ -13,7 +13,7 @@ string python_pow_to_math_pow(string s) {
   return res;
 }
 
-string math_pow_to_python_pow(string s) {
+string math_pow_to_python_pow(const string &s) {
   string res;
   for (int i = 0; i < (int)s.size(); ++i) {
     if (s[i] == '^')
@@ -24,13 +24,32 @@ string math_pow_to_python_pow(string s) {
   return res;
 }
 
+vector<int> calc_balance_array(const string& s) {
+  vector<int> b(s.size());
+  for (int i = 0; i < (int)s.size(); ++i)
+    b[i] = b[i - 1] + (s[i] == '(' ? 1 : (s[i] == ')' ? -1 : 0));
+  return b;
+}
+
+int balance(const vector<int>& b, int l, int r) {
+  return b[(int)(min_element(b.begin() + l, b.begin() + r) - b.begin())];
+}
+
+string remove_outer_brackets(const string& s) {
+  vector<int> b = calc_balance_array(s);
+  int x = 0;
+  for (; s[x] == '(' && s[s.size() - x - 1] == ')' && x + 1 < (int)s.size() - x - 1  && balance(b, x + 1, s.size() - x - 1) > x; ++x) {
+  }
+  return s.substr(x, s.size() - 2 * x);
+}
+
 int main() {
-  freopen("deriv.in", "r", stdin);
-  freopen("deriv.out", "w", stdout);
+  //freopen("deriv.in", "r", stdin);
+  //freopen("deriv.out", "w", stdout);
   string s;
   while (!cin.eof()) {
     getline(cin, s);
-    cout << python_pow_to_math_pow(s) << endl;
+    cout << remove_outer_brackets(python_pow_to_math_pow(s)) << endl;
   }
   return 0;
 }
